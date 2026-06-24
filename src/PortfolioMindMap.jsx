d78b6969466576ade21e0da0eb82d600adf6160d
ssetagayahm-lab <s.setagaya.hm@gmail.com>
2026-06-24 15:40:37 +0900

    Update PortfolioMindMap.jsx

--- src/PortfolioMindMap.jsx
+++ src/PortfolioMindMap.jsx
@@ -108,13 +108,12 @@ const NODES = [
   en: "FLYER",
   code: "FL.04",
   sub: "フライヤー / グラフィックデザイン",
-  angle: 30,
-  accent: ["#8c4a8c", "#c79bc7"],
   photos: [
-    { caption: "チラシデザイン 01" },
-    { caption: "チラシデザイン 02" },
-    { caption: "チラシデザイン 03" },
-  ],
+  { caption: "チラシデザイン 01", src: "/images/flyer01.jpg" },
+  { caption: "チラシデザイン 02" },
+  { caption: "チラシデザイン 03" },
+],
+
 },
   {
     id: "av",
